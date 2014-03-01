f3c5b99ba94e9dadec097e5a2eb3ac5861b460af
Andrew Akagawa <andrewakagawa@gmail.com>
2014-03-01 19:19:15 +0000

    rm index

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger()); 
+
+
+app.get('/', function(request, response) {
+    buf=fs.readFileSync('index.html');
+    response.send(buf.toString('utf8', 0, buf.length));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
