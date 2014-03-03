88402c90da3fa166b585701a124efc9bed48419c
Andrew Akagawa <andrewakagawa@gmail.com>
2014-03-03 06:34:50 +0000

    added public and updated web.js

--- web.js
+++ web.js
@@ -1,17 +1,6 @@
 var express = require('express');
-var fs = require('fs');
+var app = express(); 
 
-var app = express.createServer(express.logger()); 
-
-
-app.get('/', function(request, response) {
-    buf=fs.readFileSync('index.html');
-    response.send(buf.toString('utf8', 0, buf.length));
-});
-
-
-
+app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
